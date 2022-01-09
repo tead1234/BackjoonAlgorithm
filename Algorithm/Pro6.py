61bfd37134925b4892180c0791581b68a93a795c
tead1234 <tead1234@gmail.com>
2022-01-09 20:19:43 +0900

    l

--- Algorithm/Pro6.py
+++ Algorithm/Pro6.py
@@ -1,30 +1,31 @@
+from collections import deque
+
+
+def bfs(x, y, n, computers):
+    queue = deque()
+    visited = []
+    queue.append((x, y))
+
+    while queue:
+        a, b = queue.popleft()
+        visited.append((a, b))
+        for i in range(n):
+            if computers[b][i] == 1:
+                if i not in visited:
+                    queue.append((b, i))
+                    computers[b][i] = 0
+
+
+
 def solution(n, computers):
-    global answer
-    answer = n
-
-    def dfs(x, y, n, computers):
-        for i in range(4):
-            dx = [-1, 0, 1, 0]
-            dy = [0, 1, 0, -1]
-            nx = dx[i]+x
-            ny = dy[i]+y
-            if x <= -1 or y <= -1 or x >= n  or y >= n:
-                return
-            if computers[nx][ny] == 1:
-                computers[nx][ny] =0
-                dfs(ny, ny, n, computers)
-
-        dfs(nx,ny,n,computers)
-
+    answer = 0
 
     for i in range(n):
-        if computers[i][i] ==1:
-            answer+=1
-            dfs(i, i, n, computers)
+        for j in range(n):
+            if computers[i][j] == 1:
+                bfs(i, i, n, computers)
+                answer += 1
     return answer
 
 
-
-
 print(solution(4, [[1, 0, 0, 1], [0, 1, 1, 0], [0, 1, 1, 0], [1, 1, 0, 1]]))
-
