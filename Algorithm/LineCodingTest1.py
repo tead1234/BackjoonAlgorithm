901bcbadf2275b5384ffebca69636dd4442fdef8
tead1234 <tead1234@gmail.com>
2022-04-27 09:44:47 +0900

    dad

--- Algorithm/LineCodingTest1.py
+++ Algorithm/LineCodingTest1.py
@@ -2,50 +2,51 @@
 ## 코니 위치 구하는 함수 0 1 3 6
 ## y는 최초 위치
 def Coni(x, start):
-    d = [0] * 2001
+    d = [0] * 775
     d[0] = start
     d[1] = start + 1
-    for i in range(2, 2000):
+    for i in range(2, 775):
         if i >= 1:
             d[i] = d[i - 1] + i
     return d[x]
 ## 브라운은 최대한 Coni와 가깝게 움직일거임 >> 즉 Coni의 위치에서 가장 가깝게 움직일거
 
 def Brw(start,coniPlace):
-    d = [0] * 1000001
+    d = [0] * 775
     d[0] = start
     d[1] = 1 + start
-    for i in range(2,1000001):
+    for i in range(2,775):
         if coniPlace >= d[i-1] + 1:
             d[i] = d[i - 1] + 1
             if d[i] == coniPlace:
-                return d[i]
+                return i
             if coniPlace >= d[i-1] * 2:
                 d[i] = d[i-1] * 2
                 if d[i] == coniPlace:
-                    return d[i]
+                    return i
         else:
             d[i] = d[i-1] - 1
             if d[i] == coniPlace:
-                return d[i]
+                return i
     return False
 
 def solution(start, end, placeConi, placeBrw):
-    mid = (start + end) // 2
-    while(True):
-        pros = Coni(mid,placeConi)
 
-        if Brw(placeBrw,pros) == mid:
+    while(start <= end):
+        mid = (start + end) // 2
+        pros = Coni(mid,placeConi)
+        target = Brw(placeBrw,pros)
+        if target == mid:
             return mid
         ## 브라운 위치 찾기 mid시간에
 
-        if Brw(placeBrw,pros) < mid:
+        if target <= mid:
             solution(start, mid-1,placeConi, placeBrw)
 
-        if Brw(placeBrw,pros) > mid:
+        if target > mid:
             solution(mid + 1, end, placeConi, placeBrw)
 
-        if Brw(placeBrw,pros) == False:
+        if target == False:
             return -1
 
 
@@ -53,5 +54,5 @@ def solution(start, end, placeConi, placeBrw):
 placeConi= int(input("코니"))
 placeBrown = int(input("브라운"))
 
-
-print(solution(0,2000,placeConi,placeBrown))
+print(Brw(placeBrown,Coni(5,2)))
+print(solution(0,774,placeConi,placeBrown))
