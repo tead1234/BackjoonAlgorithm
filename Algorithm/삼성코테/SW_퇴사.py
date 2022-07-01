78d113d26c31a7208370c0d80d3f30c1f6260a3d
tead1234 <tead1234@gmail.com>
2022-07-02 01:54:40 +0900

    왜 자꾸 틀리단거

--- Algorithm/삼성코테/SW_퇴사.py
+++ Algorithm/삼성코테/SW_퇴사.py
@@ -1,7 +1,6 @@
-import heapq
 def valid(i,date,checked,N):
     ## 주어진 키값부터 date를 더한값이 N이하여야하고 checked의 범위안에도 False여야함
-    if (i+date) >= N:
+    if (i+date) > N:
         return False
     elif True in checked[i:i+date]:
         return False
@@ -24,15 +23,14 @@ def find_max(list):
     return key
 
 
-# N = int(input())
-N = 7
+N = int(input())
+
 answer = 0
 checked = [False] * N
 i = 0
 a = 0
-# input_list = [list(map(int, input().split())) for _ in range(N)]
-input_list = [[3, 10], [5, 20], [1, 10], [1, 20], [2, 15], [4, 40], [2, 200]]
-## 가장 가치가 큰일을 찾고 그 선택의 유효성을 검사하는 방식
+input_list = [list(map(int, input().split())) for _ in range(N)]
+
 while True:
     a += 1
     i = find_max(input_list)
@@ -41,6 +39,8 @@ while True:
         for k in range(i,i+date):
             checked[k] = True
         answer += input_list[i][1]
+        input_list[i] = [-1, -1]
+    elif valid(i,date,checked, N) == False:
         input_list[i] = [-1, -1]
     if a > N:
         break
