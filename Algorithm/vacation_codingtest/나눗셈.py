5010d1bcb286bb2e88e24537a7eb1df6640378d6
tead1234 <tead1234@gmail.com>
2022-07-13 15:24:03 +0900

    [DP] 나눗셈

---
+++ Algorithm/vacation_codingtest/나눗셈.py
@@ -0,0 +1,20 @@
+N = int(input())
+d = [0] * (N+1)
+## 1
+d[1] =0
+## 2
+d[2] = 1
+li = []
+for i in range(3, N+1):
+    if i % 2 == 0:
+        li.append((d[i//2] + 1))
+    elif i % 5 == 0:
+        li.append((d[i//5] + 1))
+    elif i % 3 == 0:
+        li.append((d[i//3]+1))
+    li.append((d[i-1]+ 1))
+    d[i] = min(li)
+    li = []
+
+
+print(d)
