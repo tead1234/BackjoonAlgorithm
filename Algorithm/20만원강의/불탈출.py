1af6932bad39b52bd5105775114cd6f88b12ba97
tead1234 <tead1234@gmail.com>
2022-07-25 20:26:59 +0900

    파이썬이 미쳤ㄷ가

--- Algorithm/20만원강의/불탈출.py
+++ Algorithm/20만원강의/불탈출.py
@@ -1,11 +1,15 @@
 from collections import deque
-T =int(input())
+
+T = int(input())
 q = deque()
 q2 = deque()
-ans = 0
-def Finfire(w,z):
-    q2.append((w,z))
-    fire[w][z] = 1
+ans = -1
+fireList = []
+def Finfire(fireList):
+    for fir in fireList:
+        q2.append(fir)
+        w, z = fir
+        fire[w][z] = 1
     while q2:
         w1, z1 = q2.popleft()
         dw = [-1, 0, 1, 0]
@@ -13,49 +17,54 @@ def Finfire(w,z):
         for k in range(4):
             W = dw[k] + w1
             Z = dz[k] + z1
-            if g[W][Z] != "#" and n>W>=0 and m>Z>=0 and fire[W][Z] == 0 :
-                q2.append((W,Z))
-                fire[W][Z] = fire[w1][z1] + 1
-            elif W == m - 1 or W == 0 or Z == n - 1 or Z == 0:
-                if g[W][Z] != '#' and fire[W][Z] == 0:
-                    fire[W][Z] = fire[w1][z1] + 1
-                    return
+            if n > W >= 0 and m > Z >= 0:
+                if g[W][Z] != "#":
+                    if fire[W][Z] == 0:
+                        q2.append((W, Z))
+                        fire[W][Z] = fire[w1][z1] + 1
+
 def bfs(start, visited):
     global ans
     q.append(start)
-    o,p = start
-    visited[o][p] = 0
+    o, p = start
+    visited[o][p] = 1
+    if o == n-1 or p == m-1 or o ==0 or p == 0:
+        ans =1
+        return
     while q:
-        x,y = q.popleft()
-        if x > m or y >m or 0> x or 0>y:
-            return
+        x, y = q.popleft()
         dx = [-1, 0, 1, 0]
         dy = [0, -1, 0, 1]
         for k in range(4):
             X = dx[k] + x
             Y = dy[k] + y
-            if (X == m-1 and Y ==0) or (X == m-1 and Y ==n-1) or  (X ==0 and Y == n-1) or (X==0 and Y ==0):
-                ans = visited[X][Y]
-                return
-            if g[X][Y] != "#" and fire[X][Y] > visited[X][Y] == 0:
-                q.append((X,Y))
+            if n>X>=0 and m>Y>=0 :
+                if X == n-1 or X == 0 or Y == m-1 or Y == 0:
+                    if g[X][Y] == ".":
+                        if fire[X][Y] > visited[x][y] + 1:
+                            ans = visited[x][y] + 1
+                            return
+            if n>X>=0 and m>Y>=0 and g[X][Y] == "." and fire[X][Y] > visited[x][y] and visited[X][Y] == 0:
+                q.append((X, Y))
                 visited[X][Y] = visited[x][y] + 1
-
-
 for _ in range(T):
-    m,n =map(int,input().split())
+    m, n = map(int, input().split())
     g = [list(input()) for _ in range(n)]
     fire = [[0] * m for _ in range(n)]
     visited = [[0] * m for _ in range(n)]
-
+    fireList = []
     for i in range(n):
         for j1 in range(m):
             if g[i][j1] == '*':
-                Finfire(i,j1)
-    print(fire)
+                fireList.append((i, j1))
+    Finfire(fireList)
     for i in range(n):
         for j in range(m):
             if g[i][j] == "@":
-                start = (i,j)
-                bfs(start,visited)
-                print(ans)
+                start = (i, j)
+                bfs(start, visited)
+                if ans == -1:
+                    print("IMPOSSIBLE")
+                else:
+                    print(ans)
+                    ans = -1
