7d86654c0c52783c1ed1c4c2acfcac034d7b077c
tead1234 <tead1234@gmail.com>
2022-07-28 09:52:02 +0900

    dfs, bfs

---
+++ Algorithm/20만원강의/dfs&bfs.py
@@ -0,0 +1,46 @@
+from collections import deque
+n,l,start = map(int,input().split())
+List = [[] for _ in range(n+1)]
+q = deque()
+for _ in range(l):
+	s,e = map(int,input().split())
+	List[s].append(e)
+	List[e].append(s)
+List = list(list(sorted(set(a))) for a in List)
+visited = [False] * n
+visited2 = [False] * n
+ans = []
+ans2 = []
+def dfs(x):
+	if visited[x-1] == True:
+		return
+	elif visited[x-1] == False:
+		visited[x-1] = True
+		ans.append(x)
+		for i in List[x]:
+			if visited[i-1] == False:
+				dfs(i)
+	return
+
+def bfs(x):
+	q.append(x)
+	ans2.append(x)
+	while q:
+		X = q.popleft()
+		if visited2[X-1] == True:
+			continue
+		else:
+			visited2[X-1] = True
+			for a in List[X]:
+				if a in q or a in ans2:
+					continue
+				else:
+					q.append(a)
+					ans2.append(a)
+dfs(start)
+for a in ans:
+	print(a, end=" ")
+print("\t")
+bfs(start)
+for a in ans2:
+	print(a, end=" ")
--- Algorithm/kt_음료수마시기.py
+++ Algorithm/kt_음료수마시기.py
@@ -1,7 +1,29 @@
-def minus(list):
-    a, b, c = list
-    list2 = [a - 1, b, c]
-    return list2
+from collections import deque
+def bfs(list, I , p):
+    q = deque()
+    visited = []
+    ans = []
+    s = 0
+    ## 각 이름표를 붙여야함
+    for i in range(len(list)):
+        visited.append(list[i][0])
+    q.append(I)
+    while q:
+        n, l, c, r = q.popleft()
+        ## 여분이 있고 p보다 열량이 낮다면
+        if l < p and visited[r] != 0:
+            p -= l
+            visited[r] -= 1
+            s += c
+            for li in list:
+                q.append(li)
+        elif l>=p :
+            s += c
+            return s
+
+
+
+
 
 ## 사용할수 있는 음료수들만 담는 배열
 ## 각 리터당 최대의 열량을 내는 애들을 우선 배치
@@ -9,11 +31,8 @@ def solution(list, p):
     ## dp문제 list는 각각 갯수, 리터, 열량으로 구성
     ## p만큼으로 만들수 있는 최대열량 배열을 만들고 p+1의 값은 남은 음료중에 가장 큰 값을 가진 애를 더하면 되나??
     ## 남은 음료수 배열,
-    d = [0] * p
-    ## mx_val = [갯수,리터,열량]
-    mx_val = sorted(list, key= lambda x: x[2], reverse= True)
-    d[0] = mx_val[0][2]
-    mx_val[0] = minus(mx_val[0])
-    d[1] =
+    for a,i in enumerate(list):
+        i.append(a)
+        print(bfs(list, i, p))
 
-print(solution([[1,3,5],[5,1,2]], 3))
+print(solution([[1,3,5],[5,1,2], [1,1,1], [3,2,2]], 3))
