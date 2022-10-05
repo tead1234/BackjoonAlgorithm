1968e0a6c3e0c3ce2853bed31fc213386bf8286f
tead1234 <tead1234@gmail.com>
2022-10-06 00:34:49 +0900

    경사로완료

--- Algorithm/삼성코테/구현_경사로.py
+++ Algorithm/삼성코테/구현_경사로.py
@@ -1,6 +1,6 @@
 import sys
 from collections import deque
-sys.stdin = open("test.txt",mode='r')
+
 
 N,L = map(int,sys.stdin.readline().split())
 MAP = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
@@ -15,21 +15,21 @@ def find(A):
         # 다른수가 나올때 까지 q에 저장
     for idx,a in enumerate(A):
         # a가 더 큰경우 같은경우
-        if a == pivot:
-            test.append(idx)
-            print(test)
-        elif a == pivot + 1:
+
+        if a == pivot + 1:
             # 기준보다 1키면 저장된 수와
+            for j in range(idx):
+                if A[j] == pivot:
+                    test.append(j)
             if len(test) >=  L:
                 for _ in range(L):
                     t = test.pop()
                     if t in installed:
                         return False
-                    print(t)
                     installed.append(t)
                 test.clear()
                 pivot = a
-                test.append(idx)
+
             else:
                 return False
         elif a+1 == pivot:
@@ -45,12 +45,15 @@ def find(A):
                     t = test2.popleft()
                     if t in installed:
                         return False
-                    print(t)
                     installed.append(t)
-                test.clear()
+
                 test2.clear()
                 pivot = a
+            else:
+                return False
 
+        elif a == pivot:
+            continue
         else:
             return False
 
@@ -60,7 +63,6 @@ def find(A):
 answer = 0
 for m in MAP:
     if find(m):
-        print(m)
         answer += 1
 List = []
 for k in range(N):
@@ -68,7 +70,6 @@ for k in range(N):
         List.append(MAP[j][k])
     # LIST를 가지고따지기
     if find(List):
-        print(List)
         answer += 1
     List = []
 
