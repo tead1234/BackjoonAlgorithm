cf0b0f466a6bfc54163e8d70b20323ba542c1418
tead1234 <tead1234@gmail.com>
2022-11-18 08:52:53 +0900

    why time fuck??

--- Algorithm/삼전이후 각성/숫자누르기.py
+++ Algorithm/삼전이후 각성/숫자누르기.py
@@ -1,92 +1,56 @@
-# 각 손가락으로 주어진 숫자를 누를때 걸리는 비용을 계산하고 적게 나온쪽으로 누르고 이동
-## 걸리는 비용계산은 bfs 로
-from collections import deque
-
+import heapq
 
 def bfs(a, b):
-    q = deque()
-    vis = [
-        [0] * 3 for _ in range(4)
+    map = [
+        [1, 7, 6, 7, 5, 4, 5, 3, 2, 3],
+        [7, 1, 2, 4, 2, 3, 5, 4, 5, 6],
+        [6, 2, 1, 2, 3, 2, 3, 5, 4, 5],
+        [7, 4, 2, 1, 5, 3, 2, 6, 5, 4],
+        [5, 2, 3, 5, 1, 2, 4, 2, 3, 5],
+        [4, 3, 2, 3, 2, 1, 2, 3, 2, 3],
+        [5, 5, 3, 2, 4, 2, 1, 5, 3, 2],
+        [3, 4, 5, 6, 2, 3, 5, 1, 2, 4],
+        [2, 5, 4, 5, 3, 2, 3, 2, 1, 2],
+        [3, 6, 5, 4, 5, 3, 2, 4, 3, 1]
     ]
-    x, y = a
-    tar_x, tar_y = b
-    if x == tar_x and y == tar_y:
-        return 1
-    vis[x][y] = 1
-    # 가중치
-    ans = 0
-    q.append((x, y, ans))
+    return map[a][b]
 
+
+def findall(q, number):
+    q2 = []
     while q:
-        i, j, weight = q.popleft()
-        ## 제자리 누르기
-        if i == tar_x and j == tar_y:
-            return weight
-        # 상하좌우
-        dir = [(-1, 0), (0, -1), (1, 0), (0, 1)]
-        for k in range(4):
-            X = dir[k][0] + i
-            Y = dir[k][1] + j
-            if 4 > X >= 0 and 3 > Y >= 0:
-                if vis[X][Y] != 1:
-                    q.append((X, Y, weight + 2))
-                    vis[X][Y] = 1
-        # 대각선
-        dir2 = [
-            (-1, -1),
-            (1, -1),
-            (-1, 1),
-            (1, 1)
-        ]
-        for k in range(4):
-            X = dir2[k][0] + i
-            Y = dir2[k][1] + j
-            if 4 > X >= 0 and 3 > Y >= 0:
-                if vis[X][Y] != 1:
-                    q.append((X, Y, weight + 3))
-                    vis[X][Y] = 1
-        # print(q)
-        # a는 좌표
+        W,L, R  = q.pop()
+        next = int(number)
+
+        A_next = bfs(L, next)
+        B_next = bfs(R, next)
+        if next == L:
+            heapq.heappush(q2, (1 + W, next, R))
+        elif next == R:
+            heapq.heappush(q2, (1 + W, L, next))
+        else:
+            heapq.heappush(q2, (A_next + W, next, R))
+            heapq.heappush(q2, (B_next + W, L, next))
+
+    return q2
 
 
-# print(bfs((0,0),(3,1)))
 def solution(numbers):
-    answer = 0
-    left = (1, 0)
-    right = (1, 2)
-    for num in numbers:
-        if num == '1':
-            target = (0, 0)
-        elif num == '2':
-            target = (0, 1)
-        elif num == '3':
-            target = (0, 2)
-        elif num == '4':
-            target = (1, 0)
-        elif num == '5':
-            target = (1, 1)
-        elif num == '6':
-            target = (1, 2)
-        elif num == '7':
-            target = (2, 0)
-        elif num == '8':
-            target = (2, 1)
-        elif num == '9':
-            target = (2, 2)
-        elif num == '0':
-            target = (3, 1)
-        elif num == '*':
-            target = (3, 0)
-        elif num == '#':
-            target = (3, 2)
-        A = bfs(left, target)
-        B = bfs(right, target)
-        print("A와 B", A, B, answer)
-        if A <= B:
-            left = target
-            answer += A
-        elif B < A:
-            right = target
-            answer += B
-    # max(bfs)
-    return answer
+
+    q = []
+    number = list(numbers)
+
+    tar = int(number[0])
+    del number[0]
+    A = bfs(4, tar)
+    B = bfs(6, tar)
+    q.append((A, tar, 6))
+    q.append((B, 4, tar))
+    for n in number:
+        q = findall(q, int(n))
+
+
+    return q[0][0]
+
+
+# print(solution("1756"))
